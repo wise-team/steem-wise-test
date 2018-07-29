6605e38047eb52831439e54784bc503df7a5605c
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-07-29 20:25:48 +0200

    Update docker run.sh config

---
+++ run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+IMAGE=gradle:4.9-jdk8
+CMD="ls && gradle -i clean test"
+
+WORKDIR="/app"
+VOLUME_WORKDIR="${PWD}:$WORKDIR"
+VOLUME_DOCKER_SOCK="/var/run/docker.sock:/var/run/docker.sock"
+VOLUME_GRADLE_CACHE="wisetest_gradlehome1:/home/gradle/.gradle"
+
+docker run -it -w $WORKDIR -v $VOLUME_WORKDIR -v $VOLUME_DOCKER_SOCK -v $VOLUME_GRADLE_CACHE $IMAGE bash -c "$CMD"
