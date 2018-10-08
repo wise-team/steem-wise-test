ba3229252d98690c43a4982a74d483dc01a28cd3
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-08 22:24:58 +0200

    feat: test steem endpoints (also in proxy mode)

--- src/live-diagnostics/steem.test.ts
+++ src/live-diagnostics/steem.test.ts
@@ -3,14 +3,18 @@ import "mocha";
 import * as fs from "fs";
 import * as _ from "lodash";
 import * as steem from "steem";
+import * as BluebirdPromise from "bluebird";
+import * as httpProxy from "http-proxy";
+import { semverCompare } from "../util/semverCompare.js";
 
+import { data as wise } from "../wise-config.gen.js";
 import { Context } from "../Context";
 import { Config } from "../config";
 
 
 export default function(config: Config, context: Context) {
-    describe("Steem using steem-js (" + __dirname + ")", function () {
-        this.timeout(6000);
+    describe.only("Steem using steem-js (" + __dirname + ")", function () {
+        this.timeout(3500);
 
         it ("Does get_block correctly for an old block", () => {
             const testBlockNum = 26194848;
@@ -25,12 +29,12 @@ export default function(config: Config, context: Context) {
         it ("Does get_block correctly for HEAD block", async () => {
             const dynamicGlobalProperties = await steem.api.getDynamicGlobalPropertiesAsync();
             if (!dynamicGlobalProperties) throw new Error("Dynamic global properties are undefined");
-            const headBlockNum = dynamicGlobalProperties.head_block_number;
+            const obtainedBlock = dynamicGlobalProperties.head_block_number - 200;
 
-            return steem.api.getBlockAsync(headBlockNum)
+            return steem.api.getBlockAsync(obtainedBlock)
             .then((block: any) => {
                 expect(block.transactions).to.be.an("array").with.length.gt(0);
-                expect(block.transactions[0].block_num).to.be.equal(headBlockNum);
+                expect(block.transactions[0].block_num).to.be.equal(obtainedBlock);
             });
         });
 
@@ -43,5 +47,94 @@ export default function(config: Config, context: Context) {
                 expect(block.transactions[0].block_num).to.be.equal(blockNum);
             });
         });
+
+        wise.config.steem.apis.forEach(api => describe ("Steem api " + api.url, () => {
+            const port = 9014;
+            let proxiedUrl: string = api.url;
+            let proxyServer: any;
+
+            const errors: any [] = [];
+            const requests: any [] = [];
+            const responses: any [] = [];
+            const assertProperRequest = async () => {
+                if (wise.config.test.live.api.testThroughProxy) {
+                    await BluebirdPromise.delay(80);
+                    if (errors.length > 0) throw new Error(errors.pop());
+                    expect(requests).to.be.an("array").with.length.gt(0);
+                    expect(responses).to.be.an("array").with.length.gt(0);
+
+                    while (errors.length > 0) errors.pop();
+                    while (requests.length > 0) requests.pop();
+                    while (responses.length > 0) responses.pop();
+                await BluebirdPromise.delay(80);
+                }
+            };
+            if (wise.config.test.live.api.testThroughProxy) before(async () => {
+                if (api.url.indexOf("http") === 0) {
+                    proxyServer = httpProxy.createProxyServer({
+                        target: api.url,
+                        changeOrigin: true,
+                    });
+                    proxiedUrl = "http://localhost:" + port;
+                }
+                else if (api.url.indexOf("ws") === 0) {
+                    proxyServer = httpProxy.createProxyServer({
+                        target: api.url,
+                        changeOrigin: true,
+                        ws: true
+                    });
+                    proxiedUrl = "ws://localhost:" + port;
+                }
+                else throw new Error("Unknown api protocol: " + api.url);
+
+                proxyServer.on("error", (err: any, req: any, res: any) => { console.error(err); errors.push(err); });
+                proxyServer.on("proxyReq", (err: any, req: any, res: any) => { /*console.log(req);*/ requests.push(req); });
+                proxyServer.on("proxyReqWs", (err: any, req: any, res: any) => { /*console.log(req);*/ requests.push(req); });
+                proxyServer.on("proxyRes", (err: any, req: any, res: any) => { /*console.log(res);*/ responses.push(res); });
+
+                // proxyServer.listen(port);
+                proxiedUrl = api.url;
+            });
+
+            it ("responds correctly to get_accounts", async () => {
+                steem.api.setOptions({ url: proxiedUrl, /*uri: proxiedUrl*/ });
+                const accounts = await steem.api.getAccountsAsync([ "nicniezgrublem" ]);
+                expect(accounts).to.be.an("array").with.length(1);
+
+                await assertProperRequest();
+            });
+
+            it ("has required minimal blockchain version", async () => {
+                steem.api.setOptions({ url: proxiedUrl, /*uri: proxiedUrl*/ });
+                const versionOpts = await steem.api.getVersionAsync();
+                expect(semverCompare(versionOpts.blockchain_version, wise.config.steem.minimalApiBlockchainVersion)).to.be.gte(0);
+
+                await assertProperRequest();
+            });
+
+            it ("has required minimal hardfork version", async () => {
+                steem.api.setOptions({ url: proxiedUrl, /*uri: proxiedUrl*/ });
+                const hfVersion = await steem.api.getHardforkVersionAsync();
+                expect(semverCompare(hfVersion, wise.config.steem.minimalApiHardforkVersion)).to.be.gte(0);
+
+                await assertProperRequest();
+            });
+
+            it ("responds correctly to get_block", async () => {
+                steem.api.setOptions({ url: proxiedUrl, /*uri: proxiedUrl*/ });
+                const dynamicGlobalProperties = await steem.api.getDynamicGlobalPropertiesAsync();
+                if (!dynamicGlobalProperties) throw new Error("Dynamic global properties are undefined");
+                const headBlockNum = dynamicGlobalProperties.head_block_number;
+
+                const block = await steem.api.getBlockAsync(headBlockNum);
+                expect(block.transactions[0].block_num).to.be.equal(headBlockNum);
+
+                 await assertProperRequest();
+            });
+
+            if (wise.config.test.live.api.testThroughProxy) after(() => {
+                proxyServer.close();
+            });
+        }));
     });
 }
