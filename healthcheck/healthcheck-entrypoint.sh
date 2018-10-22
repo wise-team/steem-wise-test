09415966ac2de747d27796b5d40a0fef6e8e3bca
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-10-22 18:09:34 +0200

    chore: debug info in healthcheck entrypoint

--- healthcheck/healthcheck-entrypoint.sh
+++ healthcheck/healthcheck-entrypoint.sh
@@ -5,14 +5,17 @@ cd /healthcheck
 
 #§ 'git clone https://github.com/' + d(data.config.repository.github.organization) + '/' + d(data.repository.name) + ' . 2>&1'
 git clone https://github.com/wise-team/steem-wise-test . 2>&1
+echo "Clone ok"
 
 #§ 'export HOSTED_LOGS_URL="' + (data.config.test.healthcheck.hostedLogs.tls === "yes" ? "https://" : "http://") + d(data.config.test.healthcheck.hostedLogs.host) + '/"'
 export HOSTED_LOGS_URL="https://test.wise.vote/"
 
 date="$(date +"%Y_%m_%d__%H_%M_%S")";
+echo "Generating uid"
 uid="$(od -x /dev/random | head -1 | awk '{OFS="-"; print $2$3,$4,$5,$6,$7$8$9}')"
 export LOG_BASE_DIR="/logs"
 export CURRENT_LOG_DIR="${LOG_BASE_DIR}/${date}-${uid}"
+echo 'Creating log directory ${CURRENT_LOG_DIR}'
 mkdir "${CURRENT_LOG_DIR}"
 
 echo "Running tests"
