62d4b4339887da25bc23272260e16e4cfef7a456
Jędrzej Lewandowski <jedrzejblew@gmail.com>
2018-11-02 16:59:44 +0100

    fix(config): fix generated config

--- src/wise-config.gen.js
+++ src/wise-config.gen.js
@@ -324,23 +324,11 @@ export const data = {
         "policies": {
           "api": {
             "name": "wise-hub-api",
-            "policy": "
-                    # Manage hub/public secrets
-                    path "secret/hub/public/*"
-                    {
-                      capabilities = ["create", "read", "update", "delete", "list"]
-                    }
-                    ",
+            "policy": "\n                    # Manage hub/public secrets\n                    path \"secret/hub/public/*\"\n                    {\n                      capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"]\n                    }\n                    ",
           },
           "daemon": {
             "name": "wise-hub-daemon",
-            "policy": "
-                    # Manage hub/public secrets
-                    path "secret/hub/public/*"
-                    {
-                      capabilities = ["create", "read", "update", "delete", "list"]
-                    }
-                    ",
+            "policy": "\n                    # Manage hub/public secrets\n                    path \"secret/hub/public/*\"\n                    {\n                      capabilities = [\"create\", \"read\", \"update\", \"delete\", \"list\"]\n                    }\n                    ",
           },
         },
       },
